24053b93a45415f2ac8502bafc9c0610dde8fdd0
Camsmith33 <csmith74704@viterbo.edu>
2024-04-18 11:47:08 -0500

    Updated imports

--- out/um/categories.py
+++ out/um/categories.py
@@ -1,11 +1,11 @@
-#import Person from UI
-#import remove_Person from UI
-#import interest from UI
-#import remove_interest from UI
-#import remove_event from UI
-#import remove_event_category from UI
-#import add_event from UI
-#import new_event_category from UI
+from person import Person
+from uuid import remove_Person
+from uuid import interest
+from uuid import remove_interest
+from uuid import remove_event
+from uuid import remove_event_category
+from uuid import add_event
+from uuid import new_event_category
 class Categories: #Create categories class to store info about categories
     def __init__(self, classname, event, person_email):
         self.classname = classname
@@ -20,15 +20,15 @@ Categories("Studies", 0, 0)
 
 #Add persons
 def Addperson(Person, interest):
-    Categories.append(interest, 0, Person.email)
+    Categories.append.person_email(interest, 0, Person.email)
 #Add new event
 def Create_event(add_event, new_event_category):
-    Categories.append(new_event_category, add_event, 0)
+    Categories.append.event(new_event_category, add_event, 0)
 #Remove person from event group
 def Removeperson(remove_Person, remove_interest):
-    Categories.remove(remove_interest, 0, remove_Person.email)
+    Categories.remove.person_email(remove_interest, 0, remove_Person.email)
 #Remove event
 def Remove_event(remove_event, remove_event_category):
-    Categories.remove(remove_event_category, remove_event, 0)
+    Categories.remove.event(remove_event_category, remove_event, 0)
 
 
